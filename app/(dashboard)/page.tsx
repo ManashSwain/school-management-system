a1deb29997dbc927365ea30bf5ed05faa9e5bbc2
ManashSwain <manash0802@gmail.com>
2026-02-07 22:02:40 +0530

    Implement sidebar ui design changes

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -9,21 +9,37 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import announcementIcon from '../../public/icons/announcement.png';
+import assignmetIcon from '../../public/icons/assignments.png';
+import attendanceIcon from '../../public/icons/attendance.png';
+import classesIcon from '../../public/icons/classes.png';
+import eventsIcon from '../../public/icons/events.png';
+import examIcon from '../../public/icons/exam.png';
+import homeIcon from '../../public/icons/homeicon.jpeg';
+import lessonsIcon from '../../public/icons/lessons.png';
+import logoutIcon from '../../public/icons/logout.png';
+import messagesIcon from '../../public/icons/messages.png';
+import parentIcon from '../../public/icons/parent.png';
+import profileIcon from '../../public/icons/profile.png'; 
+import settingsIcon from '../../public/icons/settings.png';
+import studentIcon from '../../public/icons/student.png';
+import teacherIcon from '../../public/icons/teacher.jpeg';
+import Image from "next/image";
 
 export function AppSidebar() {
     const menuItems = [
-        {icon: "",label:"Home" , href:"/", visible:["admin"]},
-        {icon: "",label:"Teachers" , href:"/teacher", visible:["admin"]},
-        {icon: "",label:"Students" , href:"/student", visible:["admin"]},
-        {icon: "",label:"Parents" , href:"/parent", visible:["admin"]},
-        {icon: "",label:"Classes" , href:"", visible:["admin"]},
-        {icon: "",label:"Lessons" , href:"", visible:["admin"]},
-        {icon: "",label:"Exams" , href:"", visible:["admin"]},
-        {icon: "",label:"Assignments" , href:"", visible:["admin"]},
-        {icon: "",label:"Attendance" , href:"", visible:["admin"]},
-        {icon: "",label:"Events" , href:"", visible:["admin"]},
-        {icon: "",label:"Messages" , href:"", visible:["admin"]},
-        {icon: "",label:"Announcements" , href:"", visible:["admin"]},
+        {icon: homeIcon ,label:"Home" , href:"/", visible:["admin"]},
+        {icon: teacherIcon,label:"Teachers" , href:"/teacher", visible:["admin"]},
+        {icon: studentIcon,label:"Students" , href:"/student", visible:["admin"]},
+        {icon: parentIcon,label:"Parents" , href:"/parent", visible:["admin"]},
+        {icon: classesIcon,label:"Classes" , href:"", visible:["admin"]},
+        {icon: lessonsIcon,label:"Lessons" , href:"", visible:["admin"]},
+        {icon: examIcon,label:"Exams" , href:"", visible:["admin"]},
+        {icon: assignmetIcon,label:"Assignments" , href:"", visible:["admin"]},
+        {icon: attendanceIcon,label:"Attendance" , href:"", visible:["admin"]},
+        {icon: eventsIcon,label:"Events" , href:"", visible:["admin"]},
+        {icon: messagesIcon,label:"Messages" , href:"", visible:["admin"]},
+        {icon: announcementIcon,label:"Announcements" , href:"", visible:["admin"]},
     ]
   return (
     <Sidebar>
@@ -40,15 +56,20 @@ export function AppSidebar() {
               <SidebarMenuButton isActive>Dashboard</SidebarMenuButton>
             </SidebarMenuItem>
 
-            <SidebarMenuItem>
-              <SidebarMenuButton>
-                {}
-              </SidebarMenuButton>
+            <SidebarMenuItem className="flex flex-col">
+              {/* <SidebarMenuButton> */}
+                {menuItems.map((item)=>{
+                  return <div key={item.label} className="flex   items-center gap-2">
+                      <Image src={item.icon} alt={item.label} width={12} height={12}/>
+                      <label htmlFor={item.label}>{item.label}</label>
+                  </div> 
+                })}
+              {/* </SidebarMenuButton> */}
             </SidebarMenuItem>
 
-            <SidebarMenuItem>
+            {/* <SidebarMenuItem>
               <SidebarMenuButton>Teachers</SidebarMenuButton>
-            </SidebarMenuItem>
+            </SidebarMenuItem> */}
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
